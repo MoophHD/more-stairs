22ff1a5309a352770b4bc7a02710b498813f6a4c
MoophHD <d_sizykh@mail.ru>
2018-04-04 20:47:29 +0300

    mock, isometric camera

---
+++ Assets/scripts/block/Block.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	public GameObject track;
+	public bool spawned;
+
+	void OnTriggerEnter(Collider other) {
+		if (spawned) return;
+
+		
+		if (other.tag == "player") {
+			spawned = true;
+		}
+	}
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	const float speed = 5;
+	private Rigidbody rb;
+	
+	void Awake() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void Start () {
+		setVelocity( true );
+	}
+
+	void setVelocity( bool isRight ) {
+		Vector3 dir = isRight ? Vector3.right : Vector3.left;
+
+		rb.velocity = dir * speed;
+	}
+}
